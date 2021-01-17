be1ac2670788f3789aa191f40b1236e209afd910
MattSchwartzkopf <schwartzkopfm@gmail.com>
2021-01-16 22:09:11 -0500

    Updated naming convention
    Fixed a few C++ naming convention errors.

--- main.cpp
+++ main.cpp
@@ -3,19 +3,18 @@
 using namespace std;
 
 // Prints binary(integer remainder)
-void printNums(int binaryCarry[], int i) {
+void PrintNums(int binaryCarry[], int i) {
     // Adds zeros to complete 4-byte integer conversion
     for(int k = 0; k < 4-i; k++)
         cout << "0";
     // Prints binary array in reverse order
-    for (int j = i - 1; j >= 0; j--) {
+    for (int j = i - 1; j >= 0; j--)
         cout << binaryCarry[j];
-    }
     cout << " ";
 }
 
 // Separate each digit and place into array
-vector<int> splitNum(int n) {
+vector<int> SplitNum(int n) {
     vector<int> arr;
     while (n > 0) {
         int digit = n%10;
@@ -27,8 +26,8 @@ vector<int> splitNum(int n) {
 }
 
 // function to convert integer to binary
-void integerToBinary(int n) {
-    vector<int> arr = splitNum(n);
+void IntegerToBinary(int n) {
+    vector<int> arr = SplitNum(n);
     for(auto& j: arr) {
         int binaryCarry[32], i = 0;
         while (j > 0) {
@@ -36,22 +35,22 @@ void integerToBinary(int n) {
             j = j / 2;
             i++;
         }
-        printNums(binaryCarry, i);
+        PrintNums(binaryCarry, i);
     }
     cout << endl;
 }
 
 // Tests integerToBinary function
 int main() {
-    integerToBinary(1);
-    integerToBinary(5);
-    integerToBinary(12);
-    integerToBinary(35);
-    integerToBinary(117);
-    integerToBinary(2);
-    integerToBinary(21967);
-    integerToBinary(123456);
-    integerToBinary(9185482);
+    IntegerToBinary(1);
+    IntegerToBinary(5);
+    IntegerToBinary(12);
+    IntegerToBinary(35);
+    IntegerToBinary(117);
+    IntegerToBinary(2);
+    IntegerToBinary(21967);
+    IntegerToBinary(123456);
+    IntegerToBinary(9185482);
     
     system("pause");
     return 0;
